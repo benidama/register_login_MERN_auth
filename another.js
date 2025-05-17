1a0ba218029b5a0048ba5197ea5fd827cf6e782c
benidama <benidama02@gmail.com>
2025-05-17 09:19:17 +0000

    adding changes

--- another.js
+++ another.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).send("Hello World! Are you ready to learn Node.js?");
 });
 app.listen(4000, () => console.log("Server is running on port 4000"));
