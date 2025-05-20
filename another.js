c41cc3e718eba052d93769ad63d4aaed8de29662
benidama <benidama02@gmail.com>
2025-05-20 16:46:20 +0000

    adding content

--- another.js
+++ another.js
@@ -1,8 +1,6 @@
-const express = require("express");
+import express from "express";
+import user from "./controllers/user.js";
 const app = express();
 
-app.get("/user", (req, res) => {
-  const user = req.params.user;
-  res.status(200).send(`Hello World! Are you ready to learn Node.js? ${user}`);
-});
+app.get("/user", user);
 app.listen(4000, () => console.log("Server is running on port 4000"));
