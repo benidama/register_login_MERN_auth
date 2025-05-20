7fb5c13ab07a77e0e184bcaa69cc5c0fae6a3f5d
benidama <benidama02@gmail.com>
2025-05-20 11:40:38 +0000

    route changed

--- another.js
+++ another.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World! Are you ready to learn Node.js?");
+app.get("/user", (req, res) => {
+  const user = req.params.user;
+  res.status(200).send(`Hello World! Are you ready to learn Node.js? ${user}`);
 });
 app.listen(4000, () => console.log("Server is running on port 4000"));
