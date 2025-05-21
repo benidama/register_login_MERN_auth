a2656f2a85f331d08a835cd91bad6d922b8156b9
benidama <benidama02@gmail.com>
2025-05-21 15:29:00 +0000

    some changes

--- another.js
+++ another.js
@@ -1,6 +1,6 @@
-import express from "express";
-import user from "./controllers/user.js";
-const app = express();
+// import express from "express";
+// import user from "./controllers/user.js";
+// const app = express();
 
-app.get("/user", user);
-app.listen(4000, () => console.log("Server is running on port 4000"));
+// app.get("/user", user);
+// app.listen(4000, () => console.log("Server is running on port 4000"));
---
+++ controllers/user.js
@@ -0,0 +1,15 @@
+// const user = {
+//   getUser: (req, res) => {
+//     const user = req.params.user;
+//     res
+//       .status(200)
+//       .send(`Hello World! Are you ready to learn Node.js? ${user}`);
+//   },
+//   getAllUsers: (req, res) => {
+//     res
+//       .status(200)
+//       .send("Hello World! Are you ready to learn Node.js? All Users");
+//   },
+// };
+
+// module.exports = user;
