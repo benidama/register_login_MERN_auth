3d875cfd077672ab6a7e4d909f08a1c081def817
benidama <benidama02@gmail.com>
2025-06-06 11:27:37 +0000

    add changes

--- server.js
+++ server.js
@@ -9,10 +9,11 @@ const app = express();
 // middleware
 app.use(express.json());
 
-// app.use("/welcome", (req, res, next) => {
-//   console.log("A new request received at" + Date.now());
-//   next();
-// });
+app.use("/welcome", (req, res, next) => {
+  console.log("A new request received at" + Date.now());
+  next();
+});
+
 app.get("/welcome", (req, res) => {
   res.send("Welcome to the Express server!");
 });
